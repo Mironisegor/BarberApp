ad6b21d510cebba28f726d42d7c92cbd40cc7a85
Mironisegor <mironove55@icloud.com>
2024-05-17 20:33:00 +0300

    Initial Commit

---
+++ BarberApp/BarberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarberAppApp.swift
+//  BarberApp
+//
+//  Created by Vladimir Fursov on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
