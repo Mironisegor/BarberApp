18360c814c6d7544aea48ef919f2482597237a50
Mironisegor <mironove55@icloud.com>
2024-05-29 11:47:42 +0300

    ,,

---
+++ BarberApp/BarberView.swift
@@ -0,0 +1,18 @@
+//
+//  BarberView.swift
+//  BarberApp
+//
+//  Created by Vladimir Fursov on 28.05.2024.
+//
+
+import SwiftUI
+
+struct BarberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarberView()
+}
---
+++ BarberApp/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BarberApp
+//
+//  Created by Vladimir Fursov on 28.05.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
