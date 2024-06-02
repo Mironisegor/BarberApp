dea47a4e16553508e5ffa131e996508c20789b62
Egor <mironove55@icloud.com>
2024-06-02 21:02:17 +0300

    /

---
+++ BarberApp/RegistartionView2.swift
@@ -0,0 +1,18 @@
+//
+//  RegistartionView2.swift
+//  BarberApp
+//
+//  Created by Gleb  on 02.06.2024.
+//
+
+import SwiftUI
+
+struct RegistartionView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistartionView2()
+}
